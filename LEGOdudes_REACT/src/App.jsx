7c9108fed1226c3804216a887e04c0024762f9fe
galaders <kjetil.engvold@outlook.com>
2026-02-10 14:36:17 +0100

    fix av mandag sur

--- LEGOdudes_REACT/src/App.jsx
+++ LEGOdudes_REACT/src/App.jsx
@@ -1,105 +1,74 @@
-import "./style/lego.css"
-import { products } from "./assets/Legodudes"
-import { useState } from "react"
+import './style/lego.css'
+import { products } from './assets/legodudes'
+import { useEffect, useState } from 'react'
+
+import Cart from './components/Cart'
+import Products from './components/Products'
+import Header from './components/Header'
+import Nav from './components/Nav'
+import CategoryTitle from './components/CategoryTitle'
+import Layout from './components/Layout'
+import { Routes, Route } from 'react-router-dom'
+
 function App() {
-
-
   const [isOpen, setIsOpen] = useState(false)
+  const [cart, setCart] = useState([])
+  const [cartQuantity, setCartQuantity] = useState(0)
+  const [totalSum, setTotalSum] = useState(0)
 
+  console.log("Cart", cart)
+/*
+  useEffect kjøres automatisk av React
+  hver gang noe i dependency-listen endres.
+*/
+  useEffect(() => {
 
-  function Header({setIsOpen}){
-    return(
-      <header>
-            <h1>
-                <a href="Index.html">
-                    <img src="website_images/LD_logo.svg" alt="LegoOdudes"/>
-                </a>
-            </h1>
-            <button id="cart-button" onClick={()=> setIsOpen((prev) => !prev)}>
-                <div id="cart-quantity">0</div>
-                <img src="website_images/legocart.svg" alt="Handlevogn" />
-            </button>
-        </header>
-    )
-  }
-
-  function Nav(){
-    return(
-      <nav>
-        <a href="#">City</a>
-        <a href="#">Ninjago</a>
-        <a href="#">Castles and Knights</a>
-        <a href="#">Marine & Pirates</a>
-        <a href="#">Movie characters</a>
-    </nav>
-    )
-  }
-
-  function CategoryTitle(){
-    return(<h2>Ninjago</h2>)
-  }
-
-  function Products({products}){
-    return(
-    <div id="product-list">
-      {products.map(p => <ProductCard key={p.prodid} p={p} />)}
+    /*
+      cart.reduce(...) brukes for å regne ut
+      totalt antall produkter i handlekurven.
       
-    </div>)
-  }
+      - sum starter på 0
+      - item er ett produkt i cart
+      - item.quantity legges til summen for hvert produkt
+    */
+    const totalQuantity = cart.reduce(
+      (sum, item) => sum + item.quantity,
+      0
+    );
 
-  function ProductCard({p}){
-    const handleClick = ()=>{
-      console.log("legg i handlekurv")
-    }
+    /*
+      Oppdaterer state cartQuantity
+      slik at UI-et kan vise riktig totalt antall
+      (f.eks. antall varer i handlekurv-ikonet).
+    */
+    setCartQuantity(totalQuantity);
 
-    return (
-      <article className="product-card">
-          <img src={`website_images/PROD_${p.imagefile}`} alt={p.title} />
-          <a href="#">{p.category}</a>
-          <h3>{p.title}</h3>
-          <p>Kr, {p.price},-</p>
-          <button onClick={handleClick}>Legg til Handlevogn</button>
-      </article>
+    /*
+      Denne useEffect-en kjører kun når cart endres,
+      fordi cart er eneste dependency.
+    */
+   const total = cart.reduce((sum, item) => sum + (item.price * item.quantity), 0)
+   setTotalSum(total)
+  }, [cart]);
+
+  function Page(){
+    return(
+          <main>
+            <CategoryTitle />
+            <Products products={products} setCart={setCart} />
+          </main>
     )
-  }
-
- function Cart({isOpen}){
-    return (
-    <section id="cart" className={isOpen ? "" : "hidden"}>
-        <table id="cart-items">
-          <tbody>
-            <tr>
-                <td>Ingen varer i handlevognen enda.</td>
-            </tr>
-          </tbody>
-        </table>
-        <p>Total pris: <span id="total-price">0</span>NOK</p>
-    </section>
-    )
-  }
-
-  function CartItem(){
-    return (
-      <tr>
-          <td className="title">${product.title}</td>
-          <td className="price">${product.price}</td>
-          <td className="quantity">${ci.qunatity}</td>
-          <td className="delete"><button onClick="deleteFromCart(${product.prodid})">X</button></td>
-      </tr>
-    )
-
   }
 
   return (
-    <div id="container">
-    <Header setIsOpen={setIsOpen} />
-    <Nav/>
-    <main>
-      <CategoryTitle/>
-      <Products products={products}/>
-    </main>
-    <Cart isOpen={isOpen} />
-    </div>
+    <Layout setIsOpen={setIsOpen} cartQuantity={cartQuantity} isOpen={isOpen} cart={cart} setCart={setCart} totalSum={totalSum}>
+      <Routes>
+        <Route index element={<Page />} />
+        <Route path='city' element={<CategoryTitle title="City" />} />
+        <Route path='ninjago' element={<CategoryTitle title="Ninjago" />} />
+      </Routes>
+    </Layout>
+
   )
 }
 
